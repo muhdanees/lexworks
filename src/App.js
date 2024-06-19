3d71c66a4c23a6d93df669f450b0c8947b93faa2
Mohd Anees <anees.office@gmail.com>
2024-06-19 18:46:26 +0530

    coming soon udpate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import ComingSoon from './Components/ComingSoon';
+import Header from './Components/Header';
+import './Styles/global.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>www.lexworks.co.in, Coming Soon!</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          2024
-        </a>
-      </header>
-    </div>
+    <ComingSoon />
+    // <Header />
   );
 }
 
---
+++ src/Components/ComingSoon.js
@@ -0,0 +1,8 @@
+export default function ComingSoon(){
+    return(
+        <div className="comingSoon">
+            <a href="www.lexworks.co.in"><img src="lexworks-logo.png" alt="lexworks" title="lexworks" /></a>
+            <div className="text"><strong><a href="www.lexworks.co.in">www.lexworks.co.in</a> - ComingSoon...</strong></div>
+        </div>
+    )
+}
