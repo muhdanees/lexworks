b3d22efcfe84d41371679bca2fcd812bdcdb03c8
Mohd Anees <anees.office@gmail.com>
2024-12-14 18:02:56 +0530

    init: astro build

--- src/Components/ComingSoon.js
+++
@@ -1,8 +0,0 @@
-export default function ComingSoon(){
-    return(
-        <div className="comingSoon">
-            <a href="./"><img src="/lexworks-logo.png" alt="lexworks" title="lexworks" /></a>
-            <div className="text"><strong><a href="./">www.lexworks.co.in</a> - ComingSoon...</strong></div>
-        </div>
-    )
-}
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <header>My header</header>
+    );
+}
+
+export default Header;
