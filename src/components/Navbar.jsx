edc1fd816243449112e54b8fcd73a26ae7082bc9
akbaruddin <akbaruddin@live.in>
2025-01-14 21:35:57 +0530

    add: title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Topbar({ page = "" }) {
               <a href="/ourteam" className={`nav-item nav-link ${page === "ourteam" ? "active": ""}`}>Our Team</a>
               <a href="/news-listing" className={`nav-item nav-link ${page === "news-listing" ? "active": ""}`}>Updates</a>
               <div className="nav-item dropdown">
-                <a href="javascript:void(0)" className="nav-link" data-bs-toggle="dropdown">
+                <a href="javascript:void(0)" className={`nav-link ${page === "practice-area" ? "active": ""}`} data-bs-toggle="dropdown">
                   <span className="dropdown-toggle">Practice Area</span>
                 </a>
                 <div className="dropdown-menu m-0">
