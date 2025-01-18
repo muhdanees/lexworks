fb2bf08fff017d1a5138e65e3649d8f65509876b
akbaruddin <akbaruddin@live.in>
2025-01-18 22:32:06 +0530

    feat: url

---
+++ src/components/PageHeaderPost.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function PageHeaderPost({
+  pageTitle = ""
+}) {
+  return (
+    <>
+      <div className="container-fluid bg-breadcrumb">
+        <div className="container text-center py-5">
+          <h3
+            className="text-white display-3 mb-4 wow fadeInDown"
+            data-wow-delay="0.1s"
+          >
+            {pageTitle}
+          </h3>
+          <ol
+            className="breadcrumb justify-content-center text-white mb-0 wow fadeInDown"
+            data-wow-delay="0.3s"
+          >
+            <li className="breadcrumb-item">
+              <a href="/" className="text-white">
+                Home
+              </a>
+            </li>
+            <li className="breadcrumb-item">
+              <a href="/news-listing" className="text-white">
+                Pages
+              </a>
+            </li>
+            <li className="breadcrumb-item active text-secondary">{pageTitle}</li>
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+}
