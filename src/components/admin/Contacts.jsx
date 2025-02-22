61ecf19de0deef0ec9b6f865df2c34d8b79f9ba6
akbaruddin <akbaruddin@live.in>
2025-02-22 22:08:40 +0530

    fix: popup

--- src/components/admin/Contacts.jsx
+++ src/components/admin/Contacts.jsx
@@ -1,6 +1,44 @@
-import { EnvelopeIcon, HashtagIcon, LinkIcon, PencilIcon, PhoneIcon, UserCircleIcon } from "@heroicons/react/24/solid";
-import React, { useEffect, useRef, useState } from "react";
-import { toast } from "react-toastify";
+import {
+  EnvelopeIcon,
+  HashtagIcon,
+  LinkIcon,
+  PencilIcon,
+  PhoneIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/solid";
+import React, { useEffect, useState } from "react";
+
+function PopUp({ url }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button
+        onClick={onClick}
+        className="text-blue-600 flex items-center gap-2"
+      >
+        <LinkIcon className="size-4" /> Document
+      </button>
+      {isOpen && (
+        <div className="fixed z-[999] inset-0 size-full flex justify-center items-center p-4 bg-black/5">
+          <div className="bg-white relative size-full rounded-lg overflow-hidden">
+            <iframe src={url} className="size-full" />
+            <button
+              onClick={onClick}
+              className="absolute right-4 top-4 px-4 py-2 border border-gray-300 rounded bg-white"
+            >
+              &times;
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
 
 export default function Contacts({ API_URL }) {
   const [contacts, setContacts] = useState([]);
@@ -28,18 +66,25 @@ export default function Contacts({ API_URL }) {
               key={`${contact.name}-${index}`}
             >
               <div className="flex justify-between items-center">
-                <div className="flex items-center gap-2"><UserCircleIcon className="size-4" /> Name: {contact.name}</div>
-                <div className="flex items-center gap-2"><EnvelopeIcon className="size-4" /> Email: {contact.email}</div>
-                <div className="flex items-center gap-2"><PhoneIcon className="size-4" /> Phone: {contact.phone}</div>
+                <div className="flex items-center gap-2">
+                  <UserCircleIcon className="size-4" /> Name: {contact.name}
+                </div>
+                <div className="flex items-center gap-2">
+                  <EnvelopeIcon className="size-4" /> Email: {contact.email}
+                </div>
+                <div className="flex items-center gap-2">
+                  <PhoneIcon className="size-4" /> Phone: {contact.phone}
+                </div>
               </div>
               <div className="mt-2 flex gap-2 items-center">
-               <HashtagIcon className="size-4" /> Subject: {contact.subject}
+                <HashtagIcon className="size-4" /> Subject: {contact.subject}
               </div>
               <div className="mt-2 flex gap-2 items-center">
-               <PencilIcon className="size-4" /> Description: {contact.description}
+                <PencilIcon className="size-4" /> Description:{" "}
+                {contact.description}
               </div>
               <div className="mt-2">
-                <a className="text-blue-600 flex items-center gap-2" target="_black" href={contact.image}><LinkIcon className="size-4" /> Document</a>
+                <PopUp url={contact.image} />
               </div>
             </div>
           ))}
